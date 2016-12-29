f6257550e53770fc1ad74f896b3430bfdaa6925d
apatrida <jayson.minard@gmail.com>
2016-12-29 18:28:04 -0300

    Send response commands as result values instead of just std out.

--- src/main/kotlin/org/jetbrains/kotlin/jupyter/commands.kt
+++ src/main/kotlin/org/jetbrains/kotlin/jupyter/commands.kt
@@ -17,7 +17,7 @@ fun runCommand(code: String, repl: ReplForJupyter?): ResponseWithMessage {
                 return ResponseWithMessage(ResponseState.Error, textResult("Failed!"), null, "unknown command: $code\nto see available commands, enter :help")
             }
     return when (cmd) {
-        ReplCommands.classpath -> ResponseWithMessage(ResponseState.OK, textResult("Ok!"), "current classpath:\n${repl?.classpath?.joinToString("\n  ", prefix = "  ")}", null)
-        ReplCommands.help -> ResponseWithMessage(ResponseState.OK, textResult("Ok!"), "Available commands:\n${ReplCommands.values().joinToString("\n    ", prefix = "    ") { ":${it.name} - ${it.desc}" }}", null)
+        ReplCommands.classpath -> ResponseWithMessage(ResponseState.Ok, textResult("current classpath:\n${repl?.classpath?.joinToString("\n  ", prefix = "  ")}"), null, null)
+        ReplCommands.help -> ResponseWithMessage(ResponseState.Ok, textResult("Available commands:\n${ReplCommands.values().joinToString("\n    ", prefix = "    ") { ":${it.name} - ${it.desc}" }}"), null, null)
     }
 }
