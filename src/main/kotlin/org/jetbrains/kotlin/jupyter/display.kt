8411ff40d9914e568df77509dda129c4c8ebd07a
apatrida <jayson.minard@gmail.com>
2016-12-29 10:48:07 -0300

    refactoring to a bit more current Kotlin 1.1 M04 version of scripting use common code from kotlin-script-util capture std err and std out and send back, doubling std out as the value result of Unit type returns

--- src/main/kotlin/org/jetbrains/kotlin/jupyter/display.kt
+++ src/main/kotlin/org/jetbrains/kotlin/jupyter/display.kt
@@ -2,10 +2,14 @@ package org.jetbrains.kotlin.jupyter
 
 import jupyter.kotlin.Result
 import jupyter.kotlin.textResult
+import org.jetbrains.kotlin.cli.common.repl.ReplEvalResult
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.PrintStream
 
-val ReplForJupyter.EvalResult.asResult: Result get() = when (this) {
-    is ReplForJupyter.EvalResult.ValueResult -> {
-        if (value is Result) value
+val ReplEvalResult.asResult: Result get() = when (this) {
+    is ReplEvalResult.ValueResult -> {
+        if (value is Result) value as Result
         else textResult(
                 try {
                     value.toString()
@@ -13,8 +17,47 @@ val ReplForJupyter.EvalResult.asResult: Result get() = when (this) {
                     "Unable to convert result to string: $e"
                 })
     }
-    is ReplForJupyter.EvalResult.UnitResult -> textResult("Ok")
-    is ReplForJupyter.EvalResult.Error -> textResult(errorText)
-    is ReplForJupyter.EvalResult.Incomplete -> textResult("error: incomplete code")
+    is ReplEvalResult.UnitResult -> textResult("Ok")
+    is ReplEvalResult.Error -> textResult(message)
+    is ReplEvalResult.Incomplete -> textResult("error: incomplete code")
     else -> throw Exception("Unexpected result from eval call: $this")
+}
+
+class ForkingOutputStream(val stdout: PrintStream, val publish: PrintStream, val captureOutput: Boolean) : OutputStream() {
+    val capturedOutput = ByteArrayOutputStream()
+
+    override fun write(b: Int) {
+        stdout.write(b)
+        publish.write(b)
+        if (captureOutput) capturedOutput.write(b)
+    }
+}
+
+fun JupyterConnection.evalWithIO(body: () -> ReplEvalResult): ReplEvalResult {
+    val out = System.out
+    val err = System.err
+
+    val forkedOut = ForkingOutputStream(out, iopubOut, true)
+    val forkedError = ForkingOutputStream(err, iopubErr, false)
+
+    System.setOut(PrintStream(forkedOut, true, "UTF-8"))
+    System.setErr(PrintStream(forkedError, true, "UTF-8"))
+
+    val `in` = System.`in`
+    System.setIn(stdinIn)
+    try {
+        return body().let {
+            if (it is ReplEvalResult.UnitResult) {
+                ReplEvalResult.ValueResult(it.updatedHistory, String(forkedOut.capturedOutput.toByteArray()))
+            } else {
+                it
+            }
+        }
+
+    }
+    finally {
+        System.setIn(`in`)
+        System.setErr(err)
+        System.setOut(out)
+    }
 }
