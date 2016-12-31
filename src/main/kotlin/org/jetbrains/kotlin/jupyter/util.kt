a2a9d25c2322fd9e7d179c6bc35ec94735532702
apatrida <jayson.minard@gmail.com>
2016-12-31 14:46:01 -0300

    fix classpath finding from a starting class to work with file based classes (as well as previously the JAR and ZIP)

--- src/main/kotlin/org/jetbrains/kotlin/jupyter/util.kt
+++ src/main/kotlin/org/jetbrains/kotlin/jupyter/util.kt
@@ -3,12 +3,13 @@ package org.jetbrains.kotlin.jupyter
 import java.io.File
 import kotlin.reflect.KClass
 
-
 fun <T: Any> KClass<T>.containingClasspath(): File? {
     val clp = "${qualifiedName?.replace('.', '/')}.class"
     val url = Thread.currentThread().contextClassLoader.getResource(clp)?.toString() ?: return null
-    val filenameRegex = """(?:zip:|jar:file:)(.*)!/(?:.*)""".toRegex()
-    val matched = filenameRegex.find(url) ?: throw IllegalStateException("Expecting a local classpath when searching for class: ${qualifiedName}")
-    val pathToJar = matched.groupValues[1]
-    return File(pathToJar)
+    val zipOrJarRegex = """(?:zip:|jar:file:)(.*)!\/(?:.*)""".toRegex()
+    val filePathRegex = """(?:file:)(.*)""".toRegex()
+    val foundPath = zipOrJarRegex.find(url)?.let { it.groupValues[1] }
+            ?: filePathRegex.find(url)?.let { it.groupValues[1].removeSuffix(clp) }
+            ?: throw IllegalStateException("Expecting a local classpath when searching for class: ${qualifiedName}")
+    return File(foundPath)
 }
