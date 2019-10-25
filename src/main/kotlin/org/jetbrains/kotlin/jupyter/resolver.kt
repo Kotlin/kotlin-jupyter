2408d7a4968cf3eee5cf7686a729baa5cd6b1b3c
Anatoly Nikitin <Anatoly.Nikitin@jetbrains.com>
2019-10-25 13:16:19 +0300

    use new dependency resolvers

--- src/main/kotlin/org/jetbrains/kotlin/jupyter/resolver.kt
+++ src/main/kotlin/org/jetbrains/kotlin/jupyter/resolver.kt
@@ -2,46 +2,39 @@ package org.jetbrains.kotlin.jupyter
 
 import jupyter.kotlin.DependsOn
 import jupyter.kotlin.Repository
-import org.jetbrains.kotlin.script.util.resolvers.DirectResolver
-import org.jetbrains.kotlin.script.util.resolvers.FlatLibDirectoryResolver
-import org.jetbrains.kotlin.script.util.resolvers.MavenResolver
-import org.jetbrains.kotlin.script.util.resolvers.experimental.*
 import java.io.File
 import kotlin.script.dependencies.ScriptContents
+import kotlin.script.experimental.*
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.asSuccess
+import kotlin.script.experimental.api.makeFailureResult
 
 open class JupyterScriptDependenciesResolver
 {
-    private var flatLibDirectoryResolver : GenericResolver = FlatLibDirectoryResolver()
+    private val resolver : GenericDependenciesResolver = CompoundDependenciesResolver(DirectDependenciesResolver(), FlatLibDirectoryDependenciesResolver(), MavenDependenciesResolver())
 
-    private var directResolver : GenericResolver = DirectResolver()
-
-    private var mavenResolver : GenericResolver = MavenResolver()
-
-    private val DependsOn.coordinates : GenericArtifactCoordinates
-            get() = MavenArtifactCoordinates(value, groupId, artifactId, version)
-
-    private val Repository.coordinates : GenericRepositoryCoordinates
-            get () = BasicRepositoryCoordinates(value.takeIf { it.isNotBlank() } ?: url, id.takeIf { it.isNotBlank() })
-
-
-    private val resolvers = sequenceOf(flatLibDirectoryResolver, directResolver, mavenResolver)
-
-    fun resolveFromAnnotations(script: ScriptContents): List<File> {
+    fun resolveFromAnnotations(script: ScriptContents): ResultWithDiagnostics<List<File>> {
         script.annotations.forEach { annotation ->
             when (annotation) {
                 is Repository -> {
-                    val coordinates = annotation.coordinates
-                    if (!flatLibDirectoryResolver.tryAddRepository(coordinates) && !mavenResolver.tryAddRepository(coordinates))
+                    if (!resolver.tryAddRepository(annotation.value))
                         throw IllegalArgumentException("Illegal argument for Repository annotation: $annotation")
                 }
                 is DependsOn -> {}
                 else -> throw Exception("Unknown annotation ${annotation.javaClass}")
             }
         }
-        return script.annotations.filterIsInstance(DependsOn::class.java).flatMap { dep ->
-            resolvers.mapNotNull { it.tryResolve(dep.coordinates) }.firstOrNull() ?:
-            throw Exception("Unable to resolve dependency $dep")
+        val scriptDiagnostics = mutableListOf<ScriptDiagnostic>()
+        val classpath = mutableListOf<File>()
+        for(annotation in script.annotations.filterIsInstance<DependsOn>()) {
+            when (val result = resolver.resolve(annotation.value)) {
+                is ResultWithDiagnostics.Failure -> scriptDiagnostics.add(ScriptDiagnostic("Failed to resolve dependencies:\n" + result.reports.joinToString("\n") { it.message }))
+                is ResultWithDiagnostics.Success -> classpath.addAll(result.value)
+            }
         }
+        return if(scriptDiagnostics.isEmpty()) classpath.asSuccess()
+               else makeFailureResult(scriptDiagnostics)
     }
 }
 
