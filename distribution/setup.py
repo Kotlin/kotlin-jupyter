920672c3a738a2a385b6cfa1b3da0a3a33a6f97d
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2019-12-19 16:38:50 +0300

    Got rid of template substitution and reduced code duplication

--- distribution/setup.py
+++ distribution/setup.py
@@ -1,7 +1,13 @@
 import glob
+from os import path
 from setuptools import setup, find_packages
 
-version = '%%VERSION%%'
+abspath = path.abspath(__file__)
+current_dir = path.dirname(abspath)
+version_file = path.join(current_dir, 'VERSION')
+
+with open(version_file, 'r') as f:
+    version = f.read().strip()
 
 DATA_FILES = [
     ('share/jupyter/kernels/kotlin', glob.glob('kernel/*.json'))
@@ -13,11 +19,12 @@ PACKAGE_DATA = {
 }
 
 if __name__ == "__main__":
-    setup(name="kotlin_jupyter_kernel",
+    setup(name="kotlin-jupyter-kernel",
           author="JetBrains",
           version=version,
-          url="https://github.com/ligee/kotlin-jupyter",
+          url="https://github.com/Kotlin/kotlin-jupyter",
           license="Apache 2.0",
+          description="Kotlin kernel for Jupyter notebooks",
           packages=find_packages(),
           package_data=PACKAGE_DATA,
           data_files=DATA_FILES
