4d8c2c3d4c7efd834f92b5c951593e553a727097
Anatoly Nikitin <Anatoly.Nikitin@jetbrains.com>
2019-12-22 22:47:39 +0300

    Add logging for resolver

--- src/main/kotlin/org/jetbrains/kotlin/jupyter/resolver.kt
+++ src/main/kotlin/org/jetbrains/kotlin/jupyter/resolver.kt
@@ -4,12 +4,21 @@ import jupyter.kotlin.DependsOn
 import jupyter.kotlin.Repository
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.kotlin.mainKts.impl.IvyResolver
+import org.slf4j.LoggerFactory
 import java.io.File
 import kotlin.script.dependencies.ScriptContents
-import kotlin.script.experimental.api.*
-import kotlin.script.experimental.dependencies.*
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.asSuccess
+import kotlin.script.experimental.api.makeFailureResult
+import kotlin.script.experimental.dependencies.CompoundDependenciesResolver
+import kotlin.script.experimental.dependencies.ExternalDependenciesResolver
+import kotlin.script.experimental.dependencies.FileSystemDependenciesResolver
+import kotlin.script.experimental.dependencies.tryAddRepository
 
 open class JupyterScriptDependenciesResolver(resolverConfig: ResolverConfig?) {
+
+    private val log by lazy { LoggerFactory.getLogger("resolver") }
 
     private val resolver: ExternalDependenciesResolver
 
@@ -33,17 +42,30 @@ open class JupyterScriptDependenciesResolver(resolverConfig: ResolverConfig?) {
         script.annotations.forEach { annotation ->
             when (annotation) {
                 is Repository -> {
+                    log.info("Adding repository: ${annotation.value}")
                     if (!resolver.tryAddRepository(annotation.value))
                         throw IllegalArgumentException("Illegal argument for Repository annotation: $annotation")
                 }
                 is DependsOn -> {
-                    val result = runBlocking { resolver.resolve(annotation.value) }
-                    when (result) {
-                        is ResultWithDiagnostics.Failure -> scriptDiagnostics.add(ScriptDiagnostic("Failed to resolve dependencies:\n" + result.reports.joinToString("\n") { it.message }))
-                        is ResultWithDiagnostics.Success -> {
-                            addedClasspath.addAll(result.value)
-                            classpath.addAll(result.value)
+                    log.info("Resolving ${annotation.value}")
+                    try {
+                        val result = runBlocking { resolver.resolve(annotation.value) }
+                        when (result) {
+                            is ResultWithDiagnostics.Failure -> {
+                                val diagnostics = ScriptDiagnostic("Failed to resolve ${annotation.value}:\n" + result.reports.joinToString("\n") { it.message })
+                                log.warn(diagnostics.message, diagnostics.exception)
+                                scriptDiagnostics.add(diagnostics)
+                            }
+                            is ResultWithDiagnostics.Success -> {
+                                log.info("Resolved: " + result.value.joinToString())
+                                addedClasspath.addAll(result.value)
+                                classpath.addAll(result.value)
+                            }
                         }
+                    } catch (e: Exception) {
+                        val diagnostic = ScriptDiagnostic("Unhandled exception during resolve", exception = e)
+                        log.error(diagnostic.message, e)
+                        scriptDiagnostics.add(diagnostic)
                     }
                 }
                 else -> throw Exception("Unknown annotation ${annotation.javaClass}")
