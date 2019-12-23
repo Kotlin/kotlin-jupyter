d54a6f74d7c434e5313fbf3fece8069bb5e8131a
Anatoly Nikitin <Anatoly.Nikitin@jetbrains.com>
2019-12-23 11:16:05 +0300

    Fix regexp for library descriptor filename

--- src/main/kotlin/org/jetbrains/kotlin/jupyter/config.kt
+++ src/main/kotlin/org/jetbrains/kotlin/jupyter/config.kt
@@ -183,7 +183,7 @@ fun downloadNewLibraryDescriptors() {
         response.jsonArray.mapNotNull {
             val o = it as JSONObject
             val filename = o["name"] as String
-            if ("""[\w-]+.$LibraryDescriptorExt""".toRegex().matches(filename)) {
+            if ("""[\w-]+\.$LibraryDescriptorExt""".toRegex().matches(filename)) {
                 val libUrl = o["download_url"].toString()
                 log.info("Downloading '$filename' from $libUrl")
                 val res = getHttp(libUrl)
