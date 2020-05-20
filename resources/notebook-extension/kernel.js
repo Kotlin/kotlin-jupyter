47bd69519c4b349b96b3dc1e707d52d2f21f47ed
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2020-05-20 22:35:51 +0300

    Get rid of scrolling selected completion variant into view

--- resources/notebook-extension/kernel.js
+++ resources/notebook-extension/kernel.js
@@ -543,7 +543,6 @@ define(function(){
 
             var active = this.sel.children()[this.selIndex];
             $(active).addClass('jp-mod-active');
-            active.scrollIntoViewIfNeeded(false);
         };
 
         Completer.prototype.keydown = function (event) {
