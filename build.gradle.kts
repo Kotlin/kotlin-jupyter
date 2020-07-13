b1ef7383bb053812f4dd28317a6df2e9896e2e25
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2020-07-14 02:50:36 +0300

    Split slf4j version from dependencies declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -164,6 +164,8 @@ val deploy: Configuration by configurations.creating
 
 dependencies {
     val junitVersion = "5.6.2"
+    val slf4jVersion = "1.7.29"
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testImplementation(kotlin("test"))
@@ -184,12 +186,12 @@ dependencies {
     compileOnly(kotlin("scripting-compiler-impl"))
 
     implementation("org.apache.maven:maven-core:3.0.3")
-    implementation("org.slf4j:slf4j-api:1.7.29")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("khttp:khttp:1.0.0")
     implementation("org.zeromq:jeromq:0.3.5")
     implementation("com.beust:klaxon:5.2")
     implementation("com.github.ajalt:clikt:2.3.0")
-    runtimeOnly("org.slf4j:slf4j-simple:1.7.29")
+    runtimeOnly("org.slf4j:slf4j-simple:$slf4jVersion")
     runtimeOnly("org.jetbrains.kotlin:jcabi-aether:1.0-dev-3") {
         exclude("org.slf4j", "slf4j-log4j12")
     }
