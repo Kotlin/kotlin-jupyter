f5ff877bbd27e7f91d3fbb378b185377024ffa36
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2020-11-05 03:12:13 +0300

    Add convenient class for providing library declarations

--- jupyter-lib/kotlin-jupyter-api/src/main/kotlin/org/jetbrains/kotlin/jupyter/api/libraryConnection.kt
+++ jupyter-lib/kotlin-jupyter-api/src/main/kotlin/org/jetbrains/kotlin/jupyter/api/libraryConnection.kt
@@ -70,6 +70,45 @@ interface LibraryDefinition {
         get() = emptyList()
 }
 
+open class FunctionalLibraryDefinition(
+    override val repositories: List<String> = emptyList(),
+    override val dependencies: List<String> = emptyList(),
+    override val imports: List<String> = emptyList(),
+) : LibraryDefinition {
+    open fun onInit(host: KotlinKernelHost): Any? {
+        return Unit
+    }
+    open fun onCellInit(host: KotlinKernelHost): Any? {
+        return Unit
+    }
+    open fun onShutdown(host: KotlinKernelHost): Any? {
+        return Unit
+    }
+    open fun render(host: KotlinKernelHost, value: Any?, resultFieldName: String?): Any? {
+        return value
+    }
+
+    override val init: List<Execution> by lazy {
+        listOf(Execution(::onInit))
+    }
+
+    override val initCell: List<Execution> by lazy {
+        listOf(Execution(::onCellInit))
+    }
+
+    override val shutdown: List<Execution> by lazy {
+        listOf(Execution(::onShutdown))
+    }
+
+    override val renderers: List<RendererTypeHandler> by lazy {
+        listOf(
+            AlwaysRendererTypeHandler { host, value, fieldName ->
+                KotlinKernelHost.Result(render(host, value, fieldName), null)
+            }
+        )
+    }
+}
+
 interface LibraryDefinitionProducer {
     fun getDefinitions(notebook: Notebook<*>?): List<LibraryDefinition>
 }
