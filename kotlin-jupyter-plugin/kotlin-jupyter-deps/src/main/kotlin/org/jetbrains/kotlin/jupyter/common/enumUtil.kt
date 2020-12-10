b6fb2b1091c433fc4634058740f6b3950a2fbae0
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2020-12-10 04:46:13 +0300

    Fix variable name (minor)

--- kotlin-jupyter-plugin/kotlin-jupyter-deps/src/main/kotlin/org/jetbrains/kotlin/jupyter/common/enumUtil.kt
+++ kotlin-jupyter-plugin/kotlin-jupyter-deps/src/main/kotlin/org/jetbrains/kotlin/jupyter/common/enumUtil.kt
@@ -1,9 +1,9 @@
 package org.jetbrains.kotlin.jupyter.common
 
-private val magicRegex = Regex("_(.)")
+private val enumNameRegex = Regex("_(.)")
 
 fun getNameForUser(name: String): String {
-    return magicRegex.replace(name.toLowerCase()) { match ->
+    return enumNameRegex.replace(name.toLowerCase()) { match ->
         match.groupValues[1].toUpperCase()
     }
 }
