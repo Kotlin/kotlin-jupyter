8102107874266420c72f7ae82cbd2f163e99757e
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2021-01-29 23:44:55 +0300

    Log errors when instantiation of library integration class fails

--- jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/config/logging.kt
+++ jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/config/logging.kt
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory
 fun getLogger(name: String = "ikotlin"): Logger = LoggerFactory.getLogger(name)
 fun <T> Logger.catchAll(msg: String = "", body: () -> T): T? = try {
     body()
-} catch (e: Exception) {
+} catch (e: Throwable) {
     this.error(msg, e)
     null
 }
