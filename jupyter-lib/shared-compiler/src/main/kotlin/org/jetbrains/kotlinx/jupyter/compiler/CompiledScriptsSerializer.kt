78d803519f9658e3009b7ecc6b2cc6f93e17afdf
Ilya Muradyan <Ilya.Muradyan@jetbrains.com>
2021-04-05 21:09:47 +0300

    Send serialized scripts for all execution artifacts, not only for main one

--- jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/compiler/CompiledScriptsSerializer.kt
+++ jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/compiler/CompiledScriptsSerializer.kt
@@ -19,25 +19,36 @@ class CompiledScriptsSerializer {
         return SerializedCompiledScriptsData(
             moduleInMemory.compilerOutputFiles.mapNotNullTo(mutableListOf()) { (name, bytes) ->
                 if (name.endsWith(".class")) {
-                    SerializedCompiledScript(name, encoder.encodeToString(bytes))
+                    SerializedCompiledScript(
+                        name,
+                        encoder.encodeToString(bytes),
+                        !name.contains("$"),
+                    )
                 } else null
             }
         )
     }
 
-    private fun deserialize(data: SerializedCompiledScriptsData): Sequence<Pair<String, ByteArray>> {
+    private fun deserialize(data: SerializedCompiledScriptsData): Sequence<Pair<SerializedCompiledScript, ByteArray>> {
         return sequence {
             for (script in data.scripts) {
-                yield(script.fileName to decoder.decode(script.data))
+                yield(script to decoder.decode(script.data))
             }
         }
     }
 
+    /**
+     * Deserializes [data] containing information about compiled scripts, saves
+     * it to the [outputDir] directory, returns the list of names of classes
+     * which are meant to be implicit receivers.
+     */
     fun deserializeAndSave(data: SerializedCompiledScriptsData, outputDir: Path): List<String> {
         val classNames = mutableListOf<String>()
-        deserialize(data).forEach { (name, bytes) ->
-            val file = outputDir.resolve(name).toFile()
-            classNames.add(file.nameWithoutExtension)
+        deserialize(data).forEach { (script, bytes) ->
+            val file = outputDir.resolve(script.fileName).toFile()
+            if (script.isImplicitReceiver) {
+                classNames.add(file.nameWithoutExtension)
+            }
             FileOutputStream(file).use { fos ->
                 BufferedOutputStream(fos).use { out ->
                     out.write(bytes)
