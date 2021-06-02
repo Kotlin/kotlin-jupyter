d331ae2c5e13ce2dc2ae85e480f5be801b05cc96
Ilya Muradyan <Ilya.Muradyan@jetbrains.com>
2021-06-02 18:50:31 +0300

    Add configuration for classloader rendering

--- jupyter-lib/lib-ext/src/main/kotlin/org/jetbrains/kotlinx/jupyter/ext/graph/wrappers/ClassLoaderNode.kt
+++ jupyter-lib/lib-ext/src/main/kotlin/org/jetbrains/kotlinx/jupyter/ext/graph/wrappers/ClassLoaderNode.kt
@@ -6,20 +6,44 @@ import org.jetbrains.kotlinx.jupyter.api.graphs.labels.TextLabel
 import java.net.URLClassLoader
 import kotlin.reflect.KClass
 
-class ClassLoaderNode(node: ClassLoader) : NodeWrapper<ClassLoader>(node) {
+class ClassLoaderRenderingConfiguration(
+    val withUrlDependencies: Boolean = true,
+    val renderer: ClassLoaderNodeRenderer = DefaultClassLoaderNodeRenderer,
+) {
+    companion object {
+        val DEFAULT = ClassLoaderRenderingConfiguration()
+    }
+}
+
+typealias ClassLoaderNodeRenderer = ClassLoaderRenderingConfiguration.(ClassLoader) -> String
+
+val DefaultClassLoaderNodeRenderer: ClassLoaderNodeRenderer = { node ->
+    when {
+        node is URLClassLoader && withUrlDependencies -> node.urLs.joinToString("\\n", "URL ClassLoader:\\n") {
+            it.toString()
+        }
+        else -> node.toString()
+    }
+}
+
+class ClassLoaderNode(
+    node: ClassLoader,
+    conf: ClassLoaderRenderingConfiguration
+) : NodeWrapper<ClassLoader>(node) {
     override val inNodes by lazy {
-        node.parent?.let { listOf(ClassLoaderNode(it)) } ?: emptyList()
+        node.parent?.let { listOf(ClassLoaderNode(it, conf)) } ?: emptyList()
     }
     override val label = TextLabel(
-        when (node) {
-            is URLClassLoader -> node.urLs.joinToString("\\n", "URL ClassLoader:\\n") {
-                it.toString()
-            }
-            else -> node.toString()
-        }
+        conf.renderer(conf, node)
     )
 }
 
-fun GraphNode.Companion.fromClassLoader(classLoader: ClassLoader) = ClassLoaderNode(classLoader)
-fun GraphNode.Companion.fromClassLoader(kClass: KClass<*>) = fromClassLoader(kClass.java.classLoader)
+fun GraphNode.Companion.fromClassLoader(
+    classLoader: ClassLoader,
+    conf: ClassLoaderRenderingConfiguration = ClassLoaderRenderingConfiguration.DEFAULT,
+) = ClassLoaderNode(classLoader, conf)
+fun GraphNode.Companion.fromClassLoader(
+    kClass: KClass<*>,
+    conf: ClassLoaderRenderingConfiguration = ClassLoaderRenderingConfiguration.DEFAULT,
+) = fromClassLoader(kClass.java.classLoader, conf)
 inline fun <reified T> GraphNode.Companion.fromClassLoader() = fromClassLoader(T::class)
