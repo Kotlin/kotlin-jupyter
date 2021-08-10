fb9dbe91218b31af062c73e7d58b1c70b09d65ef
nikolay-egorov <deathadder98@gmail.com>
2021-08-10 17:05:04 +0300

    Fix issue with recursive structures

--- jupyter-lib/api/src/main/kotlin/org/jetbrains/kotlinx/jupyter/api/VariableState.kt
+++ jupyter-lib/api/src/main/kotlin/org/jetbrains/kotlinx/jupyter/api/VariableState.kt
@@ -1,7 +1,9 @@
 package org.jetbrains.kotlinx.jupyter.api
 
+import java.lang.reflect.InvocationTargetException
 import kotlin.reflect.KProperty
 import kotlin.reflect.KProperty1
+import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.jvm.isAccessible
 
 interface VariableState {
@@ -16,6 +18,7 @@ data class VariableStateImpl(
     override val scriptInstance: Any,
 ) : VariableState {
     private var cachedValue: Any? = null
+    private var isRecursive: Boolean = false
 
     fun update() {
         val wasAccessible = property.isAccessible
@@ -23,11 +26,73 @@ data class VariableStateImpl(
         val fieldValue = property.get(scriptInstance)
         property.isAccessible = wasAccessible
         cachedValue = fieldValue
+        handleRecursiveCase()
     }
 
     override val stringValue: String?
-        get() = cachedValue?.toString()
+        get() = getProperString()
 
     override val value: Any?
         get() = cachedValue
+
+    private fun handleRecursiveCase() {
+        if (cachedValue == null) return
+        val seenNodes = mutableSetOf<Any>()
+        goThrough(cachedValue, seenNodes)
+    }
+
+    private fun getProperString(): String? {
+        fun getRecursiveObjectCode(): String {
+            val kClassName = cachedValue!!::class.simpleName
+            return buildString {
+                append("$kClassName: ")
+                append("recursive structure")
+            }
+        }
+        if (cachedValue == null) return null
+        return if (!isRecursive) {
+            cachedValue.toString()
+        } else {
+            getRecursiveObjectCode()
+        }
+    }
+
+    private fun goThrough(value: Any?, seenNodes: MutableSet<Any>) {
+        if (value == null) return
+        val membersProperties = try {
+            value::class.declaredMemberProperties
+        } catch (e: Error) {
+            emptyList<Collection<KProperty1<Any, *>>>()
+        } catch (e: Exception) {
+            emptyList<Collection<KProperty1<Any, *>>>()
+        }
+
+        val receivedInstances: MutableList<Any?> = mutableListOf()
+        for (property in membersProperties) {
+            property as KProperty1<Any, *>
+            try {
+                val wasAccessible = property.isAccessible
+                property.isAccessible = true
+                val callInstance = property.get(value)
+                property.isAccessible = wasAccessible
+                val wasSeen = if (callInstance != null) !seenNodes.add(callInstance) else false
+
+                if (wasSeen) {
+                    isRecursive = true
+                    return
+                }
+                receivedInstances.add(callInstance)
+            } catch (ex: Error) {
+                if (ex is StackOverflowError) {
+                    isRecursive = true
+                }
+                return
+            } catch (ex: InvocationTargetException) {
+                return
+            }
+        }
+        receivedInstances.forEach {
+            goThrough(it, seenNodes)
+        }
+    }
 }
