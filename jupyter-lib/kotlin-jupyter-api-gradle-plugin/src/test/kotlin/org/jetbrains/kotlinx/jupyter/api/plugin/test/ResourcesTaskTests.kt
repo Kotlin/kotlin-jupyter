984b446d5c4d6b32d7124a0b24d23397c97bcff7
Ilya Muradyan <Ilya.Muradyan@jetbrains.com>
2022-02-15 00:26:06 +0300

    Revert Kotlin version

--- jupyter-lib/kotlin-jupyter-api-gradle-plugin/src/test/kotlin/org/jetbrains/kotlinx/jupyter/api/plugin/test/ResourcesTaskTests.kt
+++ jupyter-lib/kotlin-jupyter-api-gradle-plugin/src/test/kotlin/org/jetbrains/kotlinx/jupyter/api/plugin/test/ResourcesTaskTests.kt
@@ -267,8 +267,8 @@ class ResourcesTaskTests {
     }
 
     companion object {
-        private const val KOTLIN_VERSION = "1.6.20-M1"
-        private const val KSP_VERSION = "$KOTLIN_VERSION-1.0.2"
+        private const val KOTLIN_VERSION = "1.6.0"
+        private const val KSP_VERSION = "$KOTLIN_VERSION-1.0.1"
         private const val RESOURCES_TASK_NAME = "processResources"
         private const val JUPYTER_RESOURCES_TASK_NAME = "processJupyterApiResources"
 
