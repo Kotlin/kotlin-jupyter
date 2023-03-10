dc6557440c4a661e764358019abe2e77d8392cdf
Ilya Muradyan <Ilya.Muradyan@jetbrains.com>
2023-03-10 18:16:08 +0100

    Fix GGDSL test (descriptor was updated)

--- src/test/kotlin/org/jetbrains/kotlinx/jupyter/test/repl/ReplWithTestResolverTests.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/jupyter/test/repl/ReplWithTestResolverTests.kt
@@ -80,7 +80,7 @@ class ReplWithTestResolverTests : AbstractSingleReplTest() {
             """.trimIndent(),
         )
 
-        res.metadata.newSources.shouldHaveSize(85)
+        res.metadata.newSources.shouldHaveSize(49)
     }
 
     @Test
