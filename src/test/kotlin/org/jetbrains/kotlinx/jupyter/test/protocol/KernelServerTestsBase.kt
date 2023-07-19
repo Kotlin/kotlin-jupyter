df0825ca3101493421819a38a3cfbc5008fc8268
Ilya Muradyan <Ilya.Muradyan@jetbrains.com>
2023-07-19 17:24:16 +0200

    Extract server tests execution logic into separate classes

--- src/test/kotlin/org/jetbrains/kotlinx/jupyter/test/protocol/KernelServerTestsBase.kt
+++ src/test/kotlin/org/jetbrains/kotlinx/jupyter/test/protocol/KernelServerTestsBase.kt
@@ -2,10 +2,6 @@ package org.jetbrains.kotlinx.jupyter.test.protocol
 
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
-import org.jetbrains.kotlinx.jupyter.ReplConfig
-import org.jetbrains.kotlinx.jupyter.defaultRuntimeProperties
-import org.jetbrains.kotlinx.jupyter.kernelServer
-import org.jetbrains.kotlinx.jupyter.libraries.EmptyResolutionInfoProvider
 import org.jetbrains.kotlinx.jupyter.messaging.KernelStatus
 import org.jetbrains.kotlinx.jupyter.messaging.Message
 import org.jetbrains.kotlinx.jupyter.messaging.MessageContent
@@ -23,22 +19,17 @@ import org.jetbrains.kotlinx.jupyter.protocol.createSocket
 import org.jetbrains.kotlinx.jupyter.sendMessage
 import org.jetbrains.kotlinx.jupyter.startup.createKernelPorts
 import org.jetbrains.kotlinx.jupyter.startup.createKotlinKernelConfig
-import org.jetbrains.kotlinx.jupyter.startup.javaCmdLine
 import org.jetbrains.kotlinx.jupyter.test.classpath
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestInfo
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.zeromq.ZMQ
 import java.io.File
 import java.io.IOException
 import java.net.DatagramSocket
 import java.net.ServerSocket
-import java.util.Random
-import java.util.UUID
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.concurrent.thread
 
 abstract class KernelServerTestsBase {
     protected abstract val context: ZMQ.Context
@@ -50,72 +41,29 @@ abstract class KernelServerTestsBase {
         homeDir = File(""),
     )
 
-    private val replConfig = ReplConfig.create(
-        EmptyResolutionInfoProvider,
-        kernelConfig.homeDir,
-    )
-
     private val sessionId = UUID.randomUUID().toString()
 
     private val hmac = HMAC(kernelConfig.signatureScheme, kernelConfig.signatureKey)
 
     // Set to false to debug kernel execution
     protected val runInSeparateProcess = true
-    private var serverProcess: Process? = null
-    private var serverThread: Thread? = null
+    private val executor = if (runInSeparateProcess) ProcessServerTestExecutor() else ThreadServerTestExecutor()
 
     protected val messageId = listOf(byteArrayOf(1))
-
-    private var testLogger: Logger? = null
-    private var fileOut: File? = null
-    private var fileErr: File? = null
 
     open fun beforeEach() {}
     open fun afterEach() {}
 
     @BeforeEach
     fun setupServer(testInfo: TestInfo) {
-        if (runInSeparateProcess) {
-            val testName = testInfo.displayName
-            val command = kernelConfig.javaCmdLine(javaBin, testName, classpathArg)
-
-            testLogger = LoggerFactory.getLogger("testKernel_$testName")
-            fileOut = File.createTempFile("tmp-kernel-out-$testName", ".txt")
-            fileErr = File.createTempFile("tmp-kernel-err-$testName", ".txt")
-
-            serverProcess = ProcessBuilder(command)
-                .redirectOutput(fileOut)
-                .redirectError(fileErr)
-                .start()
-        } else {
-            serverThread = thread { kernelServer(kernelConfig, replConfig, defaultRuntimeProperties) }
-        }
+        executor.setUp(testInfo, kernelConfig)
         beforeEach()
     }
 
     @AfterEach
     fun teardownServer() {
         afterEach()
-        if (runInSeparateProcess) {
-            serverProcess?.run {
-                destroy()
-                waitFor()
-            }
-            testLogger?.apply {
-                fileOut?.let {
-                    debug("Kernel output:")
-                    it.forEachLine { line -> debug(line) }
-                    it.delete()
-                }
-                fileErr?.let {
-                    debug("Kernel errors:")
-                    it.forEachLine { line -> debug(line) }
-                    it.delete()
-                }
-            }
-        } else {
-            serverThread?.interrupt()
-        }
+        executor.tearDown()
     }
 
     fun createClientSocket(socketInfo: JupyterSocketInfo) = createSocket(socketInfo, context, hmac, kernelConfig, JupyterSocketSide.CLIENT)
@@ -145,8 +93,6 @@ abstract class KernelServerTestsBase {
         private const val portRangeStart = 32768
         private const val portRangeEnd = 65536
         private const val maxTrials = portRangeEnd - portRangeStart
-        private val javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java"
-        private val classpathArg = System.getProperty("java.class.path")
 
         private fun isPortAvailable(port: Int): Boolean {
             var tcpSocket: ServerSocket? = null
---
+++ src/test/kotlin/org/jetbrains/kotlinx/jupyter/test/protocol/ServerTestExecutor.kt
@@ -0,0 +1,79 @@
+package org.jetbrains.kotlinx.jupyter.test.protocol
+
+import org.jetbrains.kotlinx.jupyter.ReplConfig
+import org.jetbrains.kotlinx.jupyter.defaultRuntimeProperties
+import org.jetbrains.kotlinx.jupyter.kernelServer
+import org.jetbrains.kotlinx.jupyter.libraries.EmptyResolutionInfoProvider
+import org.jetbrains.kotlinx.jupyter.startup.KernelConfig
+import org.jetbrains.kotlinx.jupyter.startup.javaCmdLine
+import org.junit.jupiter.api.TestInfo
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.File
+import kotlin.concurrent.thread
+
+interface ServerTestExecutor {
+    fun setUp(testInfo: TestInfo, kernelConfig: KernelConfig)
+    fun tearDown()
+}
+
+class ProcessServerTestExecutor : ServerTestExecutor {
+    private lateinit var testLogger: Logger
+    private lateinit var fileOut: File
+    private lateinit var fileErr: File
+    private lateinit var serverProcess: Process
+
+    override fun setUp(testInfo: TestInfo, kernelConfig: KernelConfig) {
+        val testName = testInfo.displayName
+        val command = kernelConfig.javaCmdLine(javaBin, testName, classpathArg)
+
+        testLogger = LoggerFactory.getLogger("testKernel_$testName")
+        fileOut = File.createTempFile("tmp-kernel-out-$testName", ".txt")
+        fileErr = File.createTempFile("tmp-kernel-err-$testName", ".txt")
+
+        serverProcess = ProcessBuilder(command)
+            .redirectOutput(fileOut)
+            .redirectError(fileErr)
+            .start()
+    }
+
+    override fun tearDown() {
+        serverProcess.run {
+            destroy()
+            waitFor()
+        }
+        testLogger.apply {
+            fileOut.let {
+                debug("Kernel output:")
+                it.forEachLine { line -> debug(line) }
+                it.delete()
+            }
+            fileErr.let {
+                debug("Kernel errors:")
+                it.forEachLine { line -> debug(line) }
+                it.delete()
+            }
+        }
+    }
+
+    companion object {
+        private val javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java"
+        private val classpathArg = System.getProperty("java.class.path")
+    }
+}
+
+class ThreadServerTestExecutor : ServerTestExecutor {
+    private lateinit var serverThread: Thread
+
+    override fun setUp(testInfo: TestInfo, kernelConfig: KernelConfig) {
+        val replConfig = ReplConfig.create(
+            EmptyResolutionInfoProvider,
+            kernelConfig.homeDir,
+        )
+        serverThread = thread { kernelServer(kernelConfig, replConfig, defaultRuntimeProperties) }
+    }
+
+    override fun tearDown() {
+        serverThread.interrupt()
+    }
+}
