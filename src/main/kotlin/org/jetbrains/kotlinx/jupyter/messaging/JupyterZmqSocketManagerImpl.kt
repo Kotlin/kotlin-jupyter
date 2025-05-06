b8240372e822b29229e4770c96d3bb1422de28d0
Arkadiy.Bazhanov <arkadiy.bazhanov@jetbrains.com>
2025-05-06 10:42:06 +0200

    Refactor Jupyter server sockets

--- src/main/kotlin/org/jetbrains/kotlinx/jupyter/messaging/JupyterZmqSocketManagerImpl.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/jupyter/messaging/JupyterZmqSocketManagerImpl.kt
@@ -1,45 +1,100 @@
 package org.jetbrains.kotlinx.jupyter.messaging
 
-import org.jetbrains.kotlinx.jupyter.api.libraries.JupyterSocketType
+import org.jetbrains.kotlinx.jupyter.api.KernelLoggerFactory
+import org.jetbrains.kotlinx.jupyter.api.getLogger
+import org.jetbrains.kotlinx.jupyter.protocol.JupyterCallbackBasedSocket
 import org.jetbrains.kotlinx.jupyter.protocol.JupyterZmqSocket
 import org.jetbrains.kotlinx.jupyter.protocol.JupyterZmqSocketInfo
+import org.jetbrains.kotlinx.jupyter.protocol.callbackBased
+import org.jetbrains.kotlinx.jupyter.protocol.openServerZmqSocket
+import org.jetbrains.kotlinx.jupyter.startup.KernelConfig
 import org.jetbrains.kotlinx.jupyter.util.closeWithTimeout
 import org.zeromq.ZMQ
+import java.io.Closeable
+import kotlin.concurrent.thread
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
 class JupyterZmqSocketManagerImpl(
+    private val loggerFactory: KernelLoggerFactory,
+    private val config: KernelConfig,
     private val terminationTimeout: Duration = 15.seconds,
-    private val openSocketAction: (JupyterZmqSocketInfo, ZMQ.Context) -> JupyterZmqSocket,
-) : JupyterZmqSocketManager {
+) : JupyterSocketManager, Closeable {
+    private val logger = loggerFactory.getLogger(this::class)
+
     private val zmqContext: ZMQ.Context = ZMQ.context(1)
 
-    private fun openSocket(socketInfo: JupyterZmqSocketInfo) = openSocketAction(socketInfo, zmqContext)
+    private val socketList = mutableListOf<Closeable>()
 
-    override val heartbeat = openSocket(JupyterZmqSocketInfo.HB)
-    override val shell = openSocket(JupyterZmqSocketInfo.SHELL)
-    override val control = openSocket(JupyterZmqSocketInfo.CONTROL)
-    override val stdin = openSocket(JupyterZmqSocketInfo.STDIN)
-    override val iopub = openSocket(JupyterZmqSocketInfo.IOPUB)
+    private fun openSocket(socketInfo: JupyterZmqSocketInfo): JupyterZmqSocket {
+        return openServerZmqSocket(loggerFactory, socketInfo, zmqContext, config)
+            .also { socketList.add(it) }
+    }
 
-    private val socketsMap =
-        buildMap {
-            put(JupyterSocketType.HB, heartbeat)
-            put(JupyterSocketType.SHELL, shell)
-            put(JupyterSocketType.CONTROL, control)
-            put(JupyterSocketType.STDIN, stdin)
-            put(JupyterSocketType.IOPUB, iopub)
+    private val heartbeat = openSocket(JupyterZmqSocketInfo.HB)
+
+    override val sockets: JupyterServerImplSockets
+
+    val shellSocket = openSocket(JupyterZmqSocketInfo.SHELL).callbackBased(logger)
+    val controlSocket = openSocket(JupyterZmqSocketInfo.CONTROL).callbackBased(logger)
+
+    init {
+        sockets = object : JupyterServerImplSockets {
+            override val shell: JupyterCallbackBasedSocket = shellSocket
+            override val control: JupyterCallbackBasedSocket = controlSocket
+            override val stdin = openSocket(JupyterZmqSocketInfo.STDIN)
+            override val iopub = openSocket(JupyterZmqSocketInfo.IOPUB)
+        }
+    }
+
+    override fun listen() {
+        val mainThread = Thread.currentThread()
+
+        fun socketLoop(
+            interruptedMessage: String,
+            vararg threadsToInterrupt: Thread,
+            loopBody: () -> Unit,
+        ) {
+            while (true) {
+                try {
+                    loopBody()
+                } catch (_: InterruptedException) {
+                    logger.debug(interruptedMessage)
+                    threadsToInterrupt.forEach { it.interrupt() }
+                    break
+                }
+            }
         }
 
-    override fun fromSocketType(type: JupyterSocketType): JupyterZmqSocket =
-        socketsMap[type] ?: throw RuntimeException("Unknown socket type: $type")
+        val controlThread = thread {
+            socketLoop("Control: Interrupted", mainThread) {
+                controlSocket.receiveMessageAndRunCallbacks()
+            }
+        }
+
+        val hbThread = thread {
+            socketLoop("Heartbeat: Interrupted", mainThread) {
+                heartbeat.let { it.zmqSocket.send(it.zmqSocket.recv()) }
+            }
+        }
+
+        socketLoop("Main: Interrupted", controlThread, hbThread) {
+            shellSocket.receiveMessageAndRunCallbacks()
+        }
+
+        try {
+            controlThread.join()
+            hbThread.join()
+        } catch (_: InterruptedException) {
+        }
+    }
 
     override fun close() {
         closeWithTimeout(terminationTimeout.inWholeMilliseconds, ::doClose)
     }
 
     private fun doClose() {
-        socketsMap.values.forEach { it.close() }
+        socketList.forEach { it.close() }
         zmqContext.close()
     }
 }
