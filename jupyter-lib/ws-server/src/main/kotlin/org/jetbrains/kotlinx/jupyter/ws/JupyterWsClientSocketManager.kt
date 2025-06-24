ffe3f30ee2bb79af87bb908a328022c83e71d2ce
Arkadiy.Bazhanov <arkadiy.bazhanov@jetbrains.com>
2025-06-24 13:08:13 +0200

    Add JupyterClientSocketManager and JupyterClientReceiveSocketManager. Add WebSocket and ZMQ implementations. Refactor ws-server module and tests.

---
+++ jupyter-lib/ws-server/src/main/kotlin/org/jetbrains/kotlinx/jupyter/ws/JupyterWsClientSocketManager.kt
@@ -0,0 +1,120 @@
+package org.jetbrains.kotlinx.jupyter.ws
+
+import org.java_websocket.WebSocket
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
+import org.jetbrains.kotlinx.jupyter.api.KernelLoggerFactory
+import org.jetbrains.kotlinx.jupyter.api.getLogger
+import org.jetbrains.kotlinx.jupyter.api.libraries.JupyterSocketType
+import org.jetbrains.kotlinx.jupyter.api.libraries.RawMessage
+import org.jetbrains.kotlinx.jupyter.messaging.JupyterClientSockets
+import org.jetbrains.kotlinx.jupyter.messaging.JupyterClientSocketManager
+import org.jetbrains.kotlinx.jupyter.protocol.JupyterCallbackBasedSocket
+import org.jetbrains.kotlinx.jupyter.startup.KernelConfig
+import org.jetbrains.kotlinx.jupyter.startup.WsKernelPorts
+import java.io.Closeable
+import java.net.URI
+import java.nio.ByteBuffer
+import java.util.EnumMap
+
+class JupyterWsClientSocketManager(
+    private val loggerFactory: KernelLoggerFactory,
+) : JupyterClientSocketManager {
+    override fun open(config: KernelConfig): JupyterClientSockets {
+        val errors = mutableListOf<Exception>()
+        return WsClientSockets(
+            loggerFactory = loggerFactory,
+            createWsClient = { messageHandler ->
+                object : WebSocketClient(
+                    URI(
+                        /* scheme = */ "ws",
+                        /* userInfo = */ null,
+                        /* host = */ config.host.takeUnless { it == "*" } ?: "0.0.0.0",
+                        /* port = */ (config.ports as WsKernelPorts).port,
+                        /* path = */ null,
+                        /* query = */ null,
+                        /* fragment = */ null,
+                    ),
+                ) {
+                    override fun onMessage(message: String) = messageHandler.onMessage(message)
+                    override fun onMessage(bytes: ByteBuffer) = messageHandler.onMessage(bytes)
+
+                    override fun onOpen(handshakedata: ServerHandshake) {}
+
+                    override fun onClose(code: Int, reason: String, remote: Boolean) {}
+
+                    override fun onError(ex: Exception) {
+                        errors.add(ex)
+                    }
+                }
+            },
+            checkErrors = { errors },
+        )
+    }
+}
+
+private class WsClientSockets(
+    private val loggerFactory: KernelLoggerFactory,
+    createWsClient: (WsMessageHandler) -> WebSocketClient,
+    checkErrors: () -> List<Exception>,
+) : JupyterClientSockets {
+    private val logger = loggerFactory.getLogger(this::class)
+    private val socketsMap = EnumMap<JupyterSocketType, WsCallbackBasedSocketImmediate>(JupyterSocketType::class.java)
+
+    private fun fromSocketType(type: JupyterSocketType) =
+        socketsMap[type] ?: throw RuntimeException("Unknown socket type: $type")
+
+    // we call `connectBlocking` only after all socket wrappers have been set up
+    private val wsClient: WebSocketClient = createWsClient(
+        WsMessageHandler(
+            logger = logger,
+            onMessageReceive = { type, message ->
+                fromSocketType(type).messageReceived(message)
+            },
+        ),
+    )
+
+    private fun createCallbackBasedSocketWrapper(type: JupyterSocketType): JupyterCallbackBasedSocket {
+        return WsCallbackBasedSocketImmediate(loggerFactory, getWebSockets = { listOf(wsClient) }, channel = type).also {
+            socketsMap[type] = it
+        }
+    }
+
+    override val shell = createCallbackBasedSocketWrapper(JupyterSocketType.SHELL)
+    override val control = createCallbackBasedSocketWrapper(JupyterSocketType.CONTROL)
+    override val ioPub = createCallbackBasedSocketWrapper(JupyterSocketType.IOPUB)
+    override val stdin = createCallbackBasedSocketWrapper(JupyterSocketType.STDIN)
+
+    init {
+        wsClient.connectBlocking()
+        val errors = checkErrors()
+        if (errors.isNotEmpty()) {
+            val thrownError = errors.first()
+            for (error in errors.drop(1)) {
+                thrownError.addSuppressed(error)
+            }
+            throw thrownError
+        }
+    }
+
+    override fun close() {
+        for (socket in socketsMap.values) {
+            socket.close()
+        }
+        wsClient.closeBlocking()
+    }
+}
+
+private class WsCallbackBasedSocketImmediate(
+    loggerFactory: KernelLoggerFactory,
+    getWebSockets: () -> Iterable<WebSocket>,
+    channel: JupyterSocketType,
+) : WsCallbackBasedSocket(loggerFactory, getWebSockets, channel), Closeable {
+    override fun messageReceived(msg: RawMessage) {
+        callbacks.runCallbacks(msg)
+    }
+
+    override fun close() {
+        callbacks.clear()
+    }
+}
---
+++ jupyter-lib/ws-server/src/main/kotlin/org/jetbrains/kotlinx/jupyter/ws/WsCallbackBasedSocket.kt
@@ -0,0 +1,85 @@
+package org.jetbrains.kotlinx.jupyter.ws
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import org.java_websocket.WebSocket
+import org.jetbrains.kotlinx.jupyter.api.KernelLoggerFactory
+import org.jetbrains.kotlinx.jupyter.api.getLogger
+import org.jetbrains.kotlinx.jupyter.api.libraries.JupyterSocketType
+import org.jetbrains.kotlinx.jupyter.api.libraries.RawMessage
+import org.jetbrains.kotlinx.jupyter.api.libraries.jupyterName
+import org.jetbrains.kotlinx.jupyter.protocol.CallbackHandler
+import org.jetbrains.kotlinx.jupyter.protocol.JupyterCallbackBasedSocket
+import org.jetbrains.kotlinx.jupyter.protocol.RawMessageCallback
+import org.jetbrains.kotlinx.jupyter.protocol.data
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+internal const val CHANNEL_FIELD_NAME = "channel"
+
+internal abstract class WsCallbackBasedSocket(
+    loggerFactory: KernelLoggerFactory,
+    private val getWebSockets: () -> Iterable<WebSocket>,
+    private val channel: JupyterSocketType,
+) : JupyterCallbackBasedSocket {
+    private val logger = loggerFactory.getLogger(this::class)
+    protected val callbacks = CallbackHandler(logger)
+
+    abstract fun messageReceived(msg: RawMessage)
+
+    override fun sendRawMessage(msg: RawMessage) {
+        val msgDataJsonString = Json.encodeToString(
+            JsonObject(msg.data + (CHANNEL_FIELD_NAME to JsonPrimitive(channel.jupyterName)))
+        )
+        for (webSocket in getWebSockets()) {
+            if (msg.id.isEmpty()) {
+                webSocket.send(msgDataJsonString)
+            } else {
+                webSocket.send(messageWithBuffersToBytes(msgDataJsonString = msgDataJsonString, buffers = msg.id))
+            }
+        }
+    }
+
+    /**
+     * See `deserialize_binary_message` in the [Jupyter server implementation](https://github.com/jupyter-server/jupyter_server/blob/main/jupyter_server/services/kernels/connection/base.py#L54-L61).
+     *
+     * Header:
+     * - 4 bytes: number of msg parts (nbufs) as 32b int
+     * - 4 * nbufs bytes: offset for each buffer as integer as 32b int
+     *
+     * Offsets are from the start of the buffer, including the header.
+     * Keep in mind that JSON document is included in the buffer count.
+     * All numbers are in big-endian format.
+     */
+    private fun messageWithBuffersToBytes(
+        msgDataJsonString: String,
+        buffers: List<ByteArray>,
+    ): ByteBuffer? {
+        val msgDataStartIndex = 4 * (2 + buffers.size)
+        val msgDataJsonBytes = msgDataJsonString.toByteArray(Charsets.UTF_8)
+        val resultBuffer = ByteBuffer.allocate(
+            /* capacity = */ msgDataStartIndex + msgDataJsonBytes.size + buffers.sumOf { it.size },
+        )
+        resultBuffer.order(ByteOrder.BIG_ENDIAN)
+        resultBuffer.putInt(buffers.size + 1)
+        run {
+            var offset = msgDataStartIndex
+            resultBuffer.putInt(offset)
+            offset += msgDataJsonBytes.size
+            for (buffer in buffers) {
+                resultBuffer.putInt(offset)
+                offset += buffer.size
+            }
+        }
+        resultBuffer.put(msgDataJsonBytes)
+        for (buffer in buffers) {
+            resultBuffer.put(buffer)
+        }
+        resultBuffer.rewind()
+        return resultBuffer
+    }
+
+    override fun onRawMessage(callback: RawMessageCallback) = callbacks.addCallback(callback)
+}
