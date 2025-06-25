3dfea20fc8e5977e530e956e8b303185f773c325
Arkadiy.Bazhanov <arkadiy.bazhanov@jetbrains.com>
2025-06-25 12:49:04 +0200

    Introduce mergeExceptions utility to help close independent resources safely

--- jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/exceptions/Util.kt
+++ jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/exceptions/Util.kt
@@ -13,3 +13,37 @@ fun Throwable.isInterruptedException(): Boolean {
         it is InterruptedException || it is ThreadDeath
     }
 }
+
+/**
+ * Runs all [ExceptionMerger.catchIndependently] blocks, even if there was an exception in one or more of them.
+ * If there were exceptions, the first one is thrown, and the others are added as suppressed to it.
+ */
+inline fun mergeExceptions(actions: ExceptionMerger.() -> Unit): Unit =
+    ExceptionMerger().apply(actions).finalize()
+
+class ExceptionMerger {
+    private var error: Throwable? = null
+
+    /** Is `true` if there was an exception in at least one [catchIndependently] block so far. */
+    val failing: Boolean get () = error != null
+
+    inline fun catchIndependently(action: () -> Unit) {
+        try {
+            action()
+        } catch (e: Throwable) {
+            addError(e)
+        }
+    }
+
+    fun addError(e: Throwable) {
+        if (error == null) {
+            error = e
+        } else {
+            error!!.addSuppressed(e)
+        }
+    }
+
+    fun finalize() {
+        error?.let { throw it }
+    }
+}
