468be17ef88436efcc57308968fd2a43a1ce6c1f
Christian Melchior <christian.melchior@jetbrains.com>
2025-09-17 15:28:20 +0200

    Add missing tests + use local build branch for testing

--- jupyter-lib/api/src/main/kotlin/org/jetbrains/kotlinx/jupyter/api/ReplCompilerMode.kt
+++ jupyter-lib/api/src/main/kotlin/org/jetbrains/kotlinx/jupyter/api/ReplCompilerMode.kt
@@ -11,7 +11,7 @@ enum class ReplCompilerMode {
     companion object
 }
 
-val ReplCompilerMode.Companion.DEFAULT get() = replCompilerModeFromEnvironment ?: ReplCompilerMode.K1
+val ReplCompilerMode.Companion.DEFAULT get() = replCompilerModeFromEnvironment ?: ReplCompilerMode.K2
 
 private val replCompilerModeFromEnvironment by lazy {
     val envValue = System.getenv("KOTLIN_JUPYTER_REPL_COMPILER_MODE") ?: return@lazy null
