f36e81bd5a2338813486fad6e8836c88993268cc
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-10-31 17:01:39 +0100

    Add test for expected transitive dependencies of the resolver

--- jupyter-lib/dependencies/dependencies-resolution-shadowed/build.gradle.kts
+++ jupyter-lib/dependencies/dependencies-resolution-shadowed/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
 
     with(project.configurations.implementation.get()) {
         exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk7")
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
         exclude("org.jetbrains.kotlin", "kotlin-scripting-common")
         exclude("org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm")
         exclude("org.jetbrains.kotlinx", "kotlinx-coroutines-bom")
@@ -24,6 +26,64 @@ dependencies {
         exclude("org.jetbrains.kotlinx", "kotlinx-serialization-json-jvm")
         exclude("org.slf4j")
     }
+}
+
+/**
+ * When changing the list, please also adjust the shadowing logic
+ */
+val expectedArtifacts =
+    listOf(
+        rootProject.name + projects.dependenciesResolution.path,
+        "org.jetbrains.amper:*",
+        "io.opentelemetry:*",
+        "io.github.pdvrieze.xmlutil:serialization-jvm",
+        "io.github.pdvrieze.xmlutil:core-jvm",
+        "org.apache.maven:maven-artifact",
+        "org.codehaus.plexus:plexus-utils",
+    )
+
+val checkResolverDependencies by tasks.registering {
+    doLast {
+        val artifacts =
+            configurations.runtimeClasspath
+                .get()
+                .resolvedConfiguration
+                .resolvedArtifacts
+
+        val expectedArtifactsSet =
+            expectedArtifacts
+                .map { it.split(":") }
+                .groupBy(keySelector = { it[0] }, valueTransform = { it[1] })
+                .mapValues { (_, artifacts) -> if ("*" in artifacts) emptySet() else artifacts.toSet() }
+
+        val artifactIds = artifacts.map { it.moduleVersion.id }
+
+        val unexpectedArtifactIds =
+            artifactIds.filterNot { id ->
+                when (val expectedArtifactsForGroup = expectedArtifactsSet[id.group]) {
+                    null -> false
+                    emptySet<String>() -> true
+                    else -> id.name in expectedArtifactsForGroup
+                }
+            }
+
+        require(unexpectedArtifactIds.isEmpty()) {
+            buildString {
+                appendLine("Some artifacts from the resolver dependency are not expected:")
+                for (id in unexpectedArtifactIds) {
+                    appendLine(" - $id")
+                }
+                appendLine("Expected artifacts:")
+                for (line in expectedArtifacts) {
+                    appendLine(" - $line")
+                }
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(checkResolverDependencies)
 }
 
 buildSettings {
