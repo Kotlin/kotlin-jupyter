1c1c2333d5b8031b3ffa4bde9d57a40d7e85d7c4
Arkadiy.Bazhanov <arkadiy.bazhanov@jetbrains.com>
2026-02-12 16:06:51 +0100

    WIP KTNB-1279: Remove duplicated compiler argument configurators and clean up related dependencies and imports

--- jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/config/Defaults.kt
+++ jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/config/Defaults.kt
@@ -3,27 +3,6 @@ package org.jetbrains.kotlinx.jupyter.config
 import org.jetbrains.kotlinx.jupyter.api.libraries.KernelRepository
 import org.jetbrains.kotlinx.jupyter.repl.MavenRepositoryCoordinates
 
-private val dateTimeImports =
-    listOf(
-        "nanoseconds",
-        "microseconds",
-        "milliseconds",
-        "seconds",
-        "minutes",
-        "hours",
-        "days",
-    ).map { "kotlin.time.Duration.Companion.$it" }
-
-val defaultGlobalImports =
-    buildList {
-        add("kotlin.math.*")
-        add("jupyter.kotlin.*")
-        add("org.jetbrains.kotlinx.jupyter.api.*")
-        add("org.jetbrains.kotlinx.jupyter.api.libraries.*")
-        add("org.jetbrains.kotlinx.jupyter.api.outputs.*")
-        addAll(dateTimeImports)
-    }
-
 val defaultRepositories =
     listOf(
         "https://repo.maven.apache.org/maven2/",
