fa47cd9c6814748cb29dc83cde354356d00bf81f
Arkadiy.Bazhanov <arkadiy.bazhanov@jetbrains.com>
2026-02-13 10:15:12 +0100

    WIP KTNB-1279: Implement deserialization for ScriptDiagnostic in ScriptDiagnosticSerializer

--- jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/messaging/serializers/ScriptDiagnosticSerializer.kt
+++ jupyter-lib/shared-compiler/src/main/kotlin/org/jetbrains/kotlinx/jupyter/messaging/serializers/ScriptDiagnosticSerializer.kt
@@ -4,20 +4,52 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
 import kotlinx.serialization.json.JsonEncoder
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.SourceCode
 
 object ScriptDiagnosticSerializer : KSerializer<ScriptDiagnostic> {
     override val descriptor: SerialDescriptor
         get() = JsonObject.serializer().descriptor
 
+    /**
+     * Deserialization produces values only suitable for serializing them back and sending them to Jupyter Web.
+     * These are not proper [ScriptDiagnostic] values.
+     */
     override fun deserialize(decoder: Decoder): ScriptDiagnostic {
-        TODO("Not yet implemented")
-    }
+        require(decoder is JsonDecoder)
 
+        val diagnosticJson = decoder.decodeJsonElement().jsonObject
+        return ScriptDiagnostic(
+            code = -1, // placeholder value
+            message = diagnosticJson.getValue("message").jsonPrimitive.content,
+            severity = ScriptDiagnostic.Severity.valueOf(diagnosticJson.getValue("severity").jsonPrimitive.content),
+            location = diagnosticJson["loc"]?.jsonObject?.let {
+                SourceCode.Location(
+                    start = SourceCode.Position(
+                        line = it.getValue("start").jsonObject.getValue("line").jsonPrimitive.int,
+                        col = it.getValue("start").jsonObject.getValue("col").jsonPrimitive.int,
+                    ),
+                    end = it["end"]?.jsonObject?.let { end ->
+                        SourceCode.Position(
+                            line = end.getValue("line").jsonPrimitive.int,
+                            col = end.getValue("col").jsonPrimitive.int,
+                        )
+                    },
+                )
+            },
+        )
+    }
+    /**
+     * Serialization produces values only suitable for sending them to Jupyter Web. Some information is lost.
+     */
     override fun serialize(
         encoder: Encoder,
         value: ScriptDiagnostic,
