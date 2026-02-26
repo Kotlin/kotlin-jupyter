6af6bcd4b8bf22ab28e6845c1d81038c6de19f3d
Arkadiy.Bazhanov <arkadiy.bazhanov@jetbrains.com>
2026-02-26 12:36:37 +0100

    WIP KTNB-1279: Add dependency on serialization plugin in kernel-compiler-impl

--- jupyter-lib/kernel-compiler-impl/build.gradle.kts
+++ jupyter-lib/kernel-compiler-impl/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation(libs.kotlin.dev.scriptRuntime)
     implementation(libs.kotlin.dev.reflect)
     implementation(libs.kotlin.dev.scriptingIdeServices)
+    runtimeOnly(libs.serialization.dev.embeddedPlugin)
 
     // trove4j is a dependency of compiler-embeddable
     implementation(libs.jetbrains.trove4j)
